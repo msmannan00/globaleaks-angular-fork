dddc6020719255662663205bbe88175436cc49ca
عبدالمنان <msmannan00@protonmail.com>
2024-10-31 12:24:20 +0500

    latest devel push

--- backend/globaleaks/tests/handlers/admin/test_tenant.py
+++ backend/globaleaks/tests/handlers/admin/test_tenant.py
@@ -14,7 +14,6 @@ def get_dummy_tenant_desc():
         'name': 'GlobaLeaks',
         'mode': 'default',
         'subdomain': 'subdomain',
-        'default_profile': 'default'
     }
 
 
@@ -54,7 +53,6 @@ class TestTenantInstance(helpers.TestHandlerWithPopulatedDB):
     def setUp(self):
         yield helpers.TestHandlerWithPopulatedDB.setUp(self)
         t = yield tenant.create(get_dummy_tenant_desc())
-        t['default_profile'] = 'default'
         self.handler = self.request(t, role='admin')
 
     def test_get(self):
